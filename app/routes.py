30b6ec4833d036de35302cc786ba124b99d15eb8
Teamo <teamorekamau97@gmail.com>
2018-10-11 23:00:19 +0300

    add user and admin pages [ch #161153149]

--- app/routes.py
+++ app/routes.py
@@ -13,9 +13,34 @@ def home():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login.html', title='login')
 
 
 @app.route('/register')
 def register():
-    return render_template('register.html')
+    return render_template('register.html', title='register')
+
+
+@app.route('/admin/myorders')
+def user_orders():
+    return render_template('user_orders.html', title='orders')
+
+
+@app.route('/admin/orders_list')
+def orders_list():
+    return render_template('orders_list.html', title='orders')
+
+
+@app.route('/admin/admin_index')
+def admin_index():
+    return render_template('admin_index.html', title='home')
+
+
+@app.route('/admin/create_menu')
+def create_menu():
+    return render_template('create_menu.html', title='create')
+
+
+@app.route('/user/create_order')
+def create_order():
+    return render_template('create_order.html', title='create')
