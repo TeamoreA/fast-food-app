5b50a5d86015787a02246dbb0043739677eef34a
Teamo <teamorekamau97@gmail.com>
2018-10-13 18:05:03 +0300

    display food menu from the api [Finishes #161153438]

---
+++ app/static/js/index_menu.js
@@ -0,0 +1,26 @@
+// display menu items
+var corsUrl = 'https://cors-anywhere.herokuapp.com/';
+const url = "https://andela-food-api.herokuapp.com/api/v2/menu";
+fetch(corsUrl + url)
+.then(function(response){
+  return response.json()
+})
+.then(function(data){
+  let items = data["menu"];
+  // console.log(items);
+  for(index = 0; index < items.length; index++){
+    document.getElementById("food_menu").innerHTML += `
+    <div class="card">
+      <img src="images/food3.jpg" class="image" alt="Avatar" width="195px" height="190" >
+      <div class="myContainer">
+        <h4><b>${items[index].name}</b></h4> 
+        <p>${items[index].description}</p> 
+        <p> <strong>price: $</strong> <em>${items[index].price}</em></p> 
+        <a class="button-small button-success" href="{{ url_for('create_order') }}">Order</a>
+     </div>
+    </div>
+    `;
+  }
+
+})
+.catch(error => console.log(error));
--- app/static/js/menu.js
+++ app/static/js/menu.js
@@ -1,38 +1,38 @@
 //post menu item
-// const token = localStorage.getItem("token");
-// menuItems = () => {
-//   var corsUrl = 'https://cors-anywhere.herokuapp.com/';
-//   let data = {
-//     name: document.getElementById("name").value,
-//     email: document.getElementById("description").value,
-//     password: document.getElementById("price").value
-//   };
-//   const url = 'https://andela-food-api.herokuapp.com/api/v2/menu';
-//   fetch(corsUrl  + url, {
-//     method: 'POST',
-//     body: JSON.stringify(data),
-//     headers: {
-//       "Content-Type": "application/json",
-//       "x-access-token": token
-//     }
-//   })
-//   .then(function(response){
-//     return response.json();
-//   })
-//   .then(function(data){
-//     let msg = data.message;
-//     if(msg === "New food item been created successfully"){
-//       document.getElementById('flash').innerHTML = msg;
-//       setTimeout(() => {document.getElementById("flash").innerHTML = "";}, 4000);
-//       window.location.href = "/menu";
-//     }
-//     else{
-//       document.getElementById('flash').innerHTML = msg;
-//       setTimeout(() => {document.getElementById("flash").innerHTML = "";}, 5000);
-//     }
-//   })
-//   .catch(error => console.log(error));
-// }
+const token = localStorage.getItem("token");
+createMenu = () => {
+  var corsUrl = 'https://cors-anywhere.herokuapp.com/';
+  let data = {
+    name: document.getElementById("name").value,
+    description: document.getElementById("description").value,
+    price: document.getElementById("price").value
+  };
+  const url = 'https://andela-food-api.herokuapp.com/api/v2/menu';
+  fetch(corsUrl  + url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+      "x-access-token": token
+    }
+  })
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    let msg = data.message;
+    if(msg === "New food item been created successfully"){
+      document.getElementById('flash').innerHTML = msg;
+      setTimeout(() => {document.getElementById("flash").innerHTML = "";}, 5000);
+      window.location.href = "/api/v2/menu";
+    }
+    else{
+      document.getElementById('flash').innerHTML = msg;
+      setTimeout(() => {document.getElementById("flash").innerHTML = "";}, 6000);
+    }
+  })
+  .catch(error => console.log(error));
+}
 
 // display menu items
 var corsUrl = 'https://cors-anywhere.herokuapp.com/';
@@ -43,18 +43,18 @@ fetch(corsUrl + url)
 })
 .then(function(data){
   let items = data["menu"];
-  console.log(items);
+  // console.log(items);
   for(i = 0; i < items.length; i++){
     document.getElementById('food_menu').innerHTML += `
+    <tr>
       <td>${items[i].id}</td>
       <td>${items[i].name}</td>
       <td>${items[i].price}</td>
       <td>
         <a class="button-small button-success" href="edit.html">Edit</a>
-        <button class="button-small button-danger" href="#" onclick="confirmDelete()">Delete</button>
+        <button class="button-small button-danger" onclick="confirmDelete()">Delete</button>
       </td>
-    `;
+    </tr>`;
   }
-
 })
 .catch(error => console.log(error));
