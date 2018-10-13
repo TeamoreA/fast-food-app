80b6b27230c2e8cf91b142939f4c5e1c8645253c
Teamo <teamorekamau97@gmail.com>
2018-10-13 12:25:52 +0300

    fix retrieve food items for admin page [ft #161153438]

--- app/routes.py
+++ app/routes.py
@@ -8,39 +8,47 @@ CORS(app)
 
 @app.route('/')
 def home():
+    '''home route'''
     return render_template('index.html')
 
 
 @app.route('/login')
 def login():
+    '''login page route'''
     return render_template('login.html', title='login')
 
 
 @app.route('/register')
 def register():
+    '''register page route'''
     return render_template('register.html', title='register')
 
 
 @app.route('/admin/myorders')
 def user_orders():
+    '''route to return orders'''
     return render_template('user_orders.html', title='orders')
 
 
 @app.route('/admin/orders_list')
 def orders_list():
+    '''method to display orders list'''
     return render_template('orders_list.html', title='orders')
 
 
 @app.route('/admin/admin_index')
 def admin_index():
+    '''method to return the admin homepage'''
     return render_template('admin_index.html', title='home')
 
 
 @app.route('/admin/create_menu')
 def create_menu():
-    return render_template('create_menu.html', title='create')
+    '''route to return the create menu page'''
+    return render_template('create_menu.html', title='create-menu')
 
 
 @app.route('/user/create_order')
 def create_order():
-    return render_template('create_order.html', title='create')
+    '''route to return create order page'''
+    return render_template('create_order.html', title='create-order')
--- app/static/js/users.js
+++ app/static/js/users.js
@@ -1,8 +1,8 @@
 //function to create a new user
 register = () => {
-  var corsUrl = 'https://morning-springs-84037.herokuapp.com/';
+  var corsUrl = 'https://cors-anywhere.herokuapp.com/';
   let data = {
-    username: document.getElementById("username").value,
+    username: document.getElementById("name").value,
     email: document.getElementById("email").value,
     password: document.getElementById("password").value,
     confirm_password: document.getElementById("confirm_password").value
@@ -16,10 +16,12 @@ register = () => {
     }
   })
   .then(function(response){
-    return response.json();
+    return response.json()
   })
   .then(function(data){
-    let msg = data.Message;
+    let msg = Object.values(data);
+    console.log(msg);
+    // let msg = data.message;
     if(msg === "New user has been created successfully"){
       document.getElementById('flash').innerHTML = msg;
       setTimeout(() => {document.getElementById("flash").innerHTML = "";}, 4000);
@@ -35,12 +37,12 @@ register = () => {
 
 //User login
 login = () => {
-  var corsUrl = 'https://morning-springs-84037.herokuapp.com/';
+  var corsUrl = 'https://cors-anywhere.herokuapp.com/';
   let data = {
     username: document.getElementById("username").value,
     password: document.getElementById("password").value
   };
-  const url = 'https://andela-food-api.herokuapp.com/api/v2/auth/login';
+  const url = 'https://andela-food-api.herokuapp.com/api/v2/auth/signin';
   fetch(corsUrl  + url, {
     method: 'POST',
     body: JSON.stringify(data),
